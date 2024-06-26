b8f5e6a808a31e07af2b41f4f23a41174175bf2c
FlorianWoelki <FlorianWoelki@gmx.de>
2024-06-26 10:26:56 +0200

    chore: add vitest

---
+++ vitest.config.ts
@@ -0,0 +1,23 @@
+import { resolve } from "path";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    setupFiles: ["./src/test-setup.ts"],
+    environment: "happy-dom",
+    coverage: {
+      all: false,
+      reporter: ["text", "json-summary", "json"],
+      provider: "istanbul",
+      thresholds: {
+        lines: 80,
+        branches: 80,
+        functions: 80,
+        statements: 80,
+      },
+    },
+  },
+  resolve: {
+    alias: [{ find: "@app", replacement: resolve(__dirname, "./src") }],
+  },
+});
