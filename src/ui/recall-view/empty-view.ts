bdc7456f6f8c0033b7ec07141e870977defb9296
FlorianWoelki <FlorianWoelki@gmx.de>
2024-06-26 12:19:51 +0200

    feat(ui): add empty view for recall view

--- src/ui/recall-view/empty-view.ts
+++ src/ui/recall-view/empty-view.ts
@@ -2,16 +2,34 @@ import { RecallView } from ".";
 import { RecallSubView } from "./sub-view";
 
 export class EmptyView extends RecallSubView {
-  private containerEl: HTMLElement;
+  private rootEl: HTMLElement;
 
   constructor(public readonly recallView: RecallView) {
     super(recallView);
   }
 
   public render(): void {
-    this.containerEl = this.recallView.rootEl.createDiv(
-      "better-recall-empty-view",
+    this.rootEl = this.recallView.rootEl.createDiv("better-recall-empty-view");
+    // Adds the `empty-state` class from Obsidian.
+    this.rootEl.addClass("empty-state");
+
+    // Uses the `empty-state-container` class from Obsidian.
+    const containerEl = this.rootEl.createDiv("empty-state-container");
+    // Uses the `empty-state-title` class from Obsidian.
+    const titleContainerEl = containerEl.createDiv("empty-state-title");
+    titleContainerEl.style.marginBottom = "10px";
+    titleContainerEl.setText("No available decks");
+
+    // Uses the `empty-state-action-list` class from Obsidian.
+    const actionListContainerEl = containerEl.createDiv(
+      "empty-state-action-list",
     );
-    this.containerEl.innerText = "Hello World!";
+    // Uses the `empty-state-action` class from Obsidian.
+    const createNewDeckActionEl =
+      actionListContainerEl.createDiv("empty-state-action");
+    createNewDeckActionEl.setText("Create new Deck");
+    createNewDeckActionEl.onClickEvent(() => {
+      // TODO: Add functionality.
+    });
   }
 }
