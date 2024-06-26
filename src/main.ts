c9f33afe0b0cae8c9fcee78875b7ae97e13f7d25
FlorianWoelki <FlorianWoelki@gmx.de>
2024-06-26 11:19:58 +0200

    feat: add loadSettings function

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { Plugin } from "obsidian";
-import { BetterRecallSettings } from "./settings/data";
+import { BetterRecallSettings, DEFAULT_SETTINGS } from "./settings/data";
 import { FILE_VIEW_TYPE, RecallView } from "./ui/recall-view";
 
 export default class BetterRecallPlugin extends Plugin {
   private data: Record<string, BetterRecallSettings>;
 
-  onload() {
+  async onload() {
     console.log("loading better recall");
+
+    await this.loadSettings();
 
     this.registerView(FILE_VIEW_TYPE, (leaf) => new RecallView(this, leaf));
     this.addCommand({
@@ -25,6 +27,25 @@ export default class BetterRecallPlugin extends Plugin {
 
   onunload() {
     console.log("unloading better recall");
+  }
+
+  /**
+   * Loads and initializes settings for the plugin.
+   * First, it loads the existing data from the plugin and then checks for any missing
+   * settings and applies default values where necessary.
+   * Finally, it populates the `data` property with this loaded data.
+   * @returns Promise that resolves when the settings have been loaded and initialized.
+   */
+  public async loadSettings(): Promise<void> {
+    const data = await this.loadData();
+    if (data) {
+      Object.entries(DEFAULT_SETTINGS).forEach(([key, value]) => {
+        if (data.settings[key] === undefined) {
+          data.settings[key] = value;
+        }
+      });
+    }
+    this.data = Object.assign({ settings: { ...DEFAULT_SETTINGS } }, {}, data);
   }
 
   public getSettings(): BetterRecallSettings {
