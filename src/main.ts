2e52a837710f7e3ad12a37f37f88838d168b58fd
FlorianWoelki <FlorianWoelki@gmx.de>
2024-06-26 10:23:45 +0200

    feat: add settings

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { Plugin } from "obsidian";
+import { BetterRecallSettings } from "./settings/data";
 
 export default class BetterRecall extends Plugin {
+  private data: Record<string, BetterRecallSettings>;
+
   onload() {
     console.log("loading better recall");
   }
 
   onunload() {
     console.log("unloading better recall");
+  }
+
+  public getSettings(): BetterRecallSettings {
+    return this.data.settings;
+  }
+
+  public async saveSettings(): Promise<void> {
+    await this.saveData(this.data);
   }
 }
---
+++ src/settings/data.ts
@@ -0,0 +1,3 @@
+export interface BetterRecallSettings {}
+
+export const DEFAULT_SETTINGS: BetterRecallSettings = {};
