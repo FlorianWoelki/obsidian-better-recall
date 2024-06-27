06f405a8355743ea6c581f8f85ba03d014c867cf
FlorianWoelki <FlorianWoelki@gmx.de>
2024-06-27 09:36:54 +0200

    test(anki): update tests and add additional tests

--- src/spaced-repetition/anki.test.ts
+++ src/spaced-repetition/anki.test.ts
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
-import { describe, it, expect, beforeEach } from 'vitest';
-import { SpacedRepetitionItem } from '.';
+import { vi, describe, it, expect, beforeEach } from 'vitest';
+import { CardState, SpacedRepetitionItem } from '.';
 import { AnkiAlgorithm, PerformanceResponse } from './anki';
 
 let ankiAlgo: AnkiAlgorithm;
@@ -12,24 +12,35 @@ function createSpacedRepetitionItem(content: string): SpacedRepetitionItem {
     easeFactor: 2.5,
     interval: 0,
     iteration: 0,
+    state: CardState.NEW,
+    stepIndex: 0,
   };
 }
 
 beforeEach(() => {
   ankiAlgo = new AnkiAlgorithm();
+  vi.useFakeTimers();
 });
 
 describe('getNextReviewItem', () => {
   it('should return `null` when no items are due', () => {
-    const item = createSpacedRepetitionItem('Test item');
-    ankiAlgo.addItem(item);
-    item.nextReviewDate = new Date(Date.now() + 10000);
+    ankiAlgo.startNewSession();
     expect(ankiAlgo.getNextReviewItem()).toBeNull();
   });
 
-  it('should return an item when it is due', () => {
+  it('should return a new item when available', () => {
     const item = createSpacedRepetitionItem('Test item');
     ankiAlgo.addItem(item);
+    ankiAlgo.startNewSession();
+    expect(ankiAlgo.getNextReviewItem()).toStrictEqual(item);
+  });
+
+  it('should return a due item when available', () => {
+    const item = createSpacedRepetitionItem('Test item');
+    ankiAlgo.addItem(item);
+    ankiAlgo.startNewSession();
+    ankiAlgo.updateItemAfterReview(item, PerformanceResponse.GOOD);
+    vi.advanceTimersByTime(1 * 60 * 1000); // Advance 1 minute
     expect(ankiAlgo.getNextReviewItem()).toStrictEqual(item);
   });
 });
@@ -37,74 +48,102 @@ describe('getNextReviewItem', () => {
 describe('updateItemAfterReview', () => {
   it('should update item correctly for AGAIN response', () => {
     const item = createSpacedRepetitionItem('Test item');
-    item.easeFactor = 2.5;
-    item.interval = 10;
     ankiAlgo.addItem(item);
+    ankiAlgo.startNewSession();
 
-    ankiAlgo.updateItemAfterReview(item, PerformanceResponse.AGAIN);
+    const reviewItem = ankiAlgo.getNextReviewItem() as SpacedRepetitionItem;
+    ankiAlgo.updateItemAfterReview(reviewItem, PerformanceResponse.AGAIN);
 
-    expect(item.easeFactor).toBe(2.3);
-    expect(item.interval).toBe(5);
-    expect(item.iteration).toBe(1);
-  });
-
-  it('should update item correctly for HARD response', () => {
-    const item = createSpacedRepetitionItem('Test item');
-    item.easeFactor = 2.5;
-    item.interval = 10;
-    ankiAlgo.addItem(item);
-
-    ankiAlgo.updateItemAfterReview(item, PerformanceResponse.HARD);
-
-    expect(item.easeFactor).toBe(2.35);
-    expect(item.interval).toBe(12);
-    expect(item.iteration).toBe(1);
+    expect(reviewItem?.state).toBe(CardState.LEARNING);
+    expect(reviewItem?.stepIndex).toBe(0);
+    expect(reviewItem?.interval).toBe(0);
   });
 
   it('should update item correctly for GOOD response', () => {
     const item = createSpacedRepetitionItem('Test item');
-    item.easeFactor = 2.5;
-    item.interval = 10;
-    item.iteration = 2;
     ankiAlgo.addItem(item);
+    ankiAlgo.startNewSession();
 
-    ankiAlgo.updateItemAfterReview(item, PerformanceResponse.GOOD);
+    let reviewItem = ankiAlgo.getNextReviewItem() as SpacedRepetitionItem;
+    ankiAlgo.updateItemAfterReview(reviewItem, PerformanceResponse.GOOD);
 
-    expect(item.easeFactor).toBe(2.5);
-    expect(item.interval).toBe(25);
-    expect(item.iteration).toBe(3);
+    expect(reviewItem?.state).toBe(CardState.LEARNING);
+    expect(reviewItem?.stepIndex).toBe(1);
+
+    vi.advanceTimersByTime(10 * 60 * 1000); // Advance 10 minutes
+
+    reviewItem = ankiAlgo.getNextReviewItem() as SpacedRepetitionItem;
+    ankiAlgo.updateItemAfterReview(reviewItem, PerformanceResponse.GOOD);
+
+    expect(reviewItem?.state).toBe(CardState.REVIEW);
+    expect(item.interval).toBe(1);
+  });
+
+  it('should update item correctly for HARD response', () => {
+    const item = createSpacedRepetitionItem('Test item');
+    item.state = CardState.REVIEW;
+    item.interval = 10;
+    ankiAlgo.addItem(item);
+    item.nextReviewDate = new Date();
+    ankiAlgo.startNewSession();
+
+    const reviewItem = ankiAlgo.getNextReviewItem() as SpacedRepetitionItem;
+    ankiAlgo.updateItemAfterReview(reviewItem, PerformanceResponse.HARD);
+
+    expect(reviewItem?.easeFactor).toBe(2.35);
+    expect(reviewItem?.interval).toBe(12);
   });
 
   it('should update item correctly for EASY response', () => {
     const item = createSpacedRepetitionItem('Test item');
-    item.easeFactor = 2.5;
+    item.state = CardState.REVIEW;
     item.interval = 10;
-    item.iteration = 2;
     ankiAlgo.addItem(item);
+    item.nextReviewDate = new Date();
+    ankiAlgo.startNewSession();
 
-    ankiAlgo.updateItemAfterReview(item, PerformanceResponse.EASY);
+    const reviewItem = ankiAlgo.getNextReviewItem() as SpacedRepetitionItem;
+    ankiAlgo.updateItemAfterReview(reviewItem, PerformanceResponse.EASY);
 
-    expect(item.easeFactor).toBe(2.65);
-    expect(item.interval).toBe(34.449999999999996);
-    expect(item.iteration).toBe(3);
+    expect(reviewItem?.easeFactor).toBe(2.65);
+    expect(reviewItem?.interval).toBeCloseTo(34.45, 2);
   });
 });
 
-describe('runReviewSession', () => {
-  it('should process due items', async () => {
-    for (let i = 0; i < 3; i++) {
-      const item = createSpacedRepetitionItem(`Test item ${i}`);
-      ankiAlgo.addItem(item);
-      item.nextReviewDate = new Date(Date.now() - 1000);
-    }
+describe('startNewSession', () => {
+  it('should reset the queue and add all due items', () => {
+    const item1 = createSpacedRepetitionItem('Item 1');
+    const item2 = createSpacedRepetitionItem('Item 2');
+    ankiAlgo.addItem(item1);
+    ankiAlgo.addItem(item2);
+    ankiAlgo.startNewSession();
 
-    await ankiAlgo.runReviewSession(3, async () => {
-      return Promise.resolve(PerformanceResponse.GOOD);
-    });
+    expect(ankiAlgo.getNextReviewItem()).toStrictEqual(item1);
+    expect(ankiAlgo.getNextReviewItem()).toStrictEqual(item2);
+    expect(ankiAlgo.getNextReviewItem()).toBeNull();
+  });
+});
 
-    ankiAlgo['items'].forEach((item) => {
-      expect(item.iteration).toBe(1);
-      expect(item.interval).toBe(1);
-    });
+describe('queuedItems behavior', () => {
+  it('should re-add items to the queue if still due after review', () => {
+    const item = createSpacedRepetitionItem('Test item');
+    ankiAlgo.addItem(item);
+    ankiAlgo.startNewSession();
+
+    const reviewItem = ankiAlgo.getNextReviewItem() as SpacedRepetitionItem;
+    ankiAlgo.updateItemAfterReview(reviewItem, PerformanceResponse.AGAIN);
+
+    expect(ankiAlgo.getNextReviewItem()).toStrictEqual(reviewItem);
+  });
+
+  it('should not re-add items to the queue if not due after review', () => {
+    const item = createSpacedRepetitionItem('Test item');
+    ankiAlgo.addItem(item);
+    ankiAlgo.startNewSession();
+
+    const reviewItem = ankiAlgo.getNextReviewItem() as SpacedRepetitionItem;
+    ankiAlgo.updateItemAfterReview(reviewItem, PerformanceResponse.EASY);
+
+    expect(ankiAlgo.getNextReviewItem()).toBeNull();
   });
 });
