ec7c38950deb0c9af3c7f17fe74936d3596ef28f
FlorianWoelki <FlorianWoelki@gmx.de>
2024-07-05 13:14:26 +0200

    refactor(data): use types for event definitions

--- src/data/event/events.ts
+++ src/data/event/events.ts
@@ -2,12 +2,31 @@ import type { SpacedRepetitionItem } from '../../spaced-repetition';
 import type { Deck } from '../deck';
 import type { Event } from './';
 
+export type EditItemEvent = Event<{
+  deckId: string;
+  newItem: SpacedRepetitionItem;
+}>;
+
+export type AddDeckEvent = Event<{ deck: Deck }>;
+
+export type EditDeckEvent = Event<{ deck: Deck }>;
+
+export type AddItemEvent = Event<{
+  deckId: string;
+  item: SpacedRepetitionItem;
+}>;
+
+export type DeleteItemEvent = Event<{
+  deckId: string;
+  deletedItem: SpacedRepetitionItem;
+}>;
+
 export type EventMap = {
-  addDeck: Event<{ deck: Deck }>;
-  editDeck: Event<{ deck: Deck }>;
-  addItem: Event<{ deckId: string; item: SpacedRepetitionItem }>;
-  editItem: Event<{ deckId: string; newItem: SpacedRepetitionItem }>;
-  deleteItem: Event<{ deckId: string; deletedItem: SpacedRepetitionItem }>;
+  addDeck: AddDeckEvent;
+  editDeck: EditDeckEvent;
+  addItem: AddItemEvent;
+  editItem: EditItemEvent;
+  deleteItem: DeleteItemEvent;
 };
 
 export type EventType = keyof EventMap;
