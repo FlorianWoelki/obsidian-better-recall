7468ac346abdf9fe9e1cc97ff7f5ddcb8e5be581
FlorianWoelki <FlorianWoelki@gmx.de>
2024-07-15 09:35:57 +0200

    feat(ui): add description to anki parameter settings

--- src/ui/settings/SettingsTab.ts
+++ src/ui/settings/SettingsTab.ts
@@ -4,17 +4,59 @@ import { ResetButtonComponent } from '../components/ResetButtonComponent';
 import { AnkiParameters, DEFAULT_SETTINGS } from 'src/settings/data';
 
 export class SettingsTab extends PluginSettingTab {
-  private titleParameterMapping: Record<string, keyof AnkiParameters> = {
-    'Lapse Interval': 'lapseInterval',
-    'Easy Interval': 'easyInterval',
-    'Easy Bonus': 'easyBonus',
-    'Graduating Interval': 'graduatingInterval',
-    'Min Ease Factor': 'minEaseFactor',
-    'Ease Factor Decrement': 'easeFactorDecrement',
-    'Ease Factor Increment': 'easeFactorIncrement',
-    'Hard Interval Multiplier': 'hardIntervalMultiplier',
-    'Learning Steps': 'learningSteps',
-    'Relearning Steps': 'relearningSteps',
+  private titleParameterMapping: Record<
+    string,
+    { description: string; parameter: keyof AnkiParameters }
+  > = {
+    'Lapse Interval': {
+      parameter: 'lapseInterval',
+      description:
+        'The multiplier applied to the current interval when a card lapses.',
+    },
+    'Easy Interval': {
+      parameter: 'easyInterval',
+      description:
+        'The interval (in days) assigned to a card when rated as `easy` during learning/relearning.',
+    },
+    'Easy Bonus': {
+      parameter: 'easyBonus',
+      description:
+        'The multiplier applied to the interval when a review card is rated as `easy`.',
+    },
+    'Graduating Interval': {
+      parameter: 'graduatingInterval',
+      description:
+        'The interval (in days) assigned to a card when it graduates from learning to review.',
+    },
+    'Min Ease Factor': {
+      parameter: 'minEaseFactor',
+      description: 'The minimum allowed ease factor for a card.',
+    },
+    'Ease Factor Decrement': {
+      parameter: 'easeFactorDecrement',
+      description:
+        'The amount by which the ease factor is decreased when a card is rated as `again`.',
+    },
+    'Ease Factor Increment': {
+      parameter: 'easeFactorIncrement',
+      description:
+        'The amount by which the ease factor is increased when a card is rated as `easy`.',
+    },
+    'Hard Interval Multiplier': {
+      parameter: 'hardIntervalMultiplier',
+      description:
+        'The multiplier applied to the current interval when a review card is rated as `hard`.',
+    },
+    'Learning Steps': {
+      parameter: 'learningSteps',
+      description:
+        'Comma-separated step intervals (in minutes) for new cards in the learning phase.',
+    },
+    'Relearning Steps': {
+      parameter: 'relearningSteps',
+      description:
+        'Comma-separated step intervals (in minutes) for cards in the relearning phase.',
+    },
   };
 
   constructor(private plugin: BetterRecallPlugin) {
@@ -26,52 +68,56 @@ export class SettingsTab extends PluginSettingTab {
 
     new Setting(this.containerEl).setName('Anki Settings').setHeading();
 
-    Object.entries(this.titleParameterMapping).forEach(([key, parameter]) => {
-      let textComponent: TextComponent | null = null;
-      const pluginValue = this.plugin.getSettings().ankiParameters[parameter];
+    Object.entries(this.titleParameterMapping).forEach(
+      ([key, { parameter, description }]) => {
+        let textComponent: TextComponent | null = null;
+        const pluginValue = this.plugin.getSettings().ankiParameters[parameter];
 
-      const setting = new Setting(this.containerEl).setName(key);
+        const setting = new Setting(this.containerEl)
+          .setName(key)
+          .setDesc(description);
 
-      new ResetButtonComponent(setting.controlEl).onClick(async () => {
-        if (!textComponent) {
-          return;
-        }
-
-        const defaultValue = DEFAULT_SETTINGS.ankiParameters[parameter];
-        this.setValue(textComponent, defaultValue);
-        this.plugin.setAnkiParameter(parameter, defaultValue);
-        await this.plugin.savePluginData();
-      });
-
-      setting.addText((text) => {
-        textComponent = text;
-        this.setValue(text, pluginValue);
-
-        text.onChange(async (input) => {
-          input = input.trim();
-          if (
-            parameter === 'learningSteps' ||
-            parameter === 'relearningSteps'
-          ) {
-            if (!this.isStringValidArray(input)) {
-              console.log('not a valid array', input);
-              return;
-            }
-
-            const newValue = this.parseStringToArray(input);
-            this.plugin.setAnkiParameter(parameter, newValue);
-          } else {
-            if (isNaN(+input)) {
-              return;
-            }
-
-            this.plugin.setAnkiParameter(parameter, Number(input));
+        new ResetButtonComponent(setting.controlEl).onClick(async () => {
+          if (!textComponent) {
+            return;
           }
 
+          const defaultValue = DEFAULT_SETTINGS.ankiParameters[parameter];
+          this.setValue(textComponent, defaultValue);
+          this.plugin.setAnkiParameter(parameter, defaultValue);
           await this.plugin.savePluginData();
         });
-      });
-    });
+
+        setting.addText((text) => {
+          textComponent = text;
+          this.setValue(text, pluginValue);
+
+          text.onChange(async (input) => {
+            input = input.trim();
+            if (
+              parameter === 'learningSteps' ||
+              parameter === 'relearningSteps'
+            ) {
+              if (!this.isStringValidArray(input)) {
+                console.log('not a valid array', input);
+                return;
+              }
+
+              const newValue = this.parseStringToArray(input);
+              this.plugin.setAnkiParameter(parameter, newValue);
+            } else {
+              if (isNaN(+input)) {
+                return;
+              }
+
+              this.plugin.setAnkiParameter(parameter, Number(input));
+            }
+
+            await this.plugin.savePluginData();
+          });
+        });
+      },
+    );
   }
 
   private setValue(text: TextComponent, value: number | number[]): void {
