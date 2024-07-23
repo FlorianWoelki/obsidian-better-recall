0b376d8b68934786a334fe61addd347d91ce3302
FlorianWoelki <FlorianWoelki@gmx.de>
2024-07-23 08:42:22 +0200

    refactor: remove prefix from commands

--- src/commands.ts
+++ src/commands.ts
@@ -3,14 +3,14 @@ import { AddCardModal } from './ui/modals/card-modal/AddCardModal';
 
 export function registerCommands(plugin: BetterRecallPlugin): void {
   plugin.addCommand({
-    id: 'better-recall-recall-view',
+    id: 'recall-view',
     name: 'Recall',
     callback: () => {
       plugin.openRecallView();
     },
   });
   plugin.addCommand({
-    id: 'better-recall-add-card',
+    id: 'add-card',
     name: 'Add Card',
     callback: () => {
       new AddCardModal(plugin).open();
